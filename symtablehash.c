23e46a82ca3b23abf50ac5e9615a812006e0cef8
jonah <jonah.johnson@princeton.edu>
2024-10-26 17:19:29 -0400

    seg faults

--- symtablehash.c
+++ symtablehash.c
@@ -171,7 +171,7 @@ static SymTable_T SymTable_rehash(SymTable_T oSymTable)
    }
 
    for (hashNum = 0; 
-         hashNum < abucketCount[oSymTable->bucketLevel - 1]; hashNum++) 
+         hashNum < abucketCount[oSymTable->bucketLevel - 1] - 1; hashNum++) 
       {
          
          for (psCurrentBinding = 
